e54b660e6e68d07a732177ab725a82218abb8226
cheongaya <chengalono@gmail.com>
2022-07-02 21:24:44 +0900

    Add : 전략패턴 활용 코드 추가

---
+++ ch01_strategy_pattern/src/practice/behabior/Impl/RunNoWayImpl.java
@@ -0,0 +1,10 @@
+package practice.behabior.Impl;
+
+import practice.behabior.RunBehavior;
+
+public class RunNoWayImpl implements RunBehavior {
+    @Override
+    public void 뛰다() {
+        System.out.println("뛸 수 없다");
+    }
+}
---
+++ ch01_strategy_pattern/src/practice/behabior/Impl/SwimSlowImpl.java
@@ -0,0 +1,10 @@
+package practice.behabior.Impl;
+
+import practice.behabior.SwimBehavior;
+
+public class SwimSlowImpl implements SwimBehavior {
+    @Override
+    public void 수영하다() {
+        System.out.println("느리게 수영한다");
+    }
+}
